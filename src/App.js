8ec82611ff5769a56c413662bc6de1ba958a6427
unknown <tonycarpenter@gmail.com>
2022-03-24 18:41:06 -0400

    Add Room Details View

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./Components/Navbar/NavBar";
 import RenterResultsContainer from "./Components/RenterResultsContainer/RenterResultsContainer"
+import RoomView from "./Components/RoomView/RoomView";
 import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <RenterResultsContainer />
+      {/* <RenterResultsContainer /> */}
+      <RoomView />
       <Footer />
     </div>
   );
