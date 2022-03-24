1a4a0ff3b26ef4eae91a372d1aee5f67cd0c0b35
Thomas Nguyen <tommi.t.nguyen@icloud.com>
2022-03-24 18:53:02 -0400

    Setup routes and pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import logo from "./logo.svg";
+import React from 'react';
+import Home from './Pages/Home/Home.js';
+import Booking from './Pages/Booking/Booking.js';
+import Dashboard from './Pages/Dashboard/Dashboard.js';
+import Profile from './Pages/Profile/Profile.js';
+import Error from './Pages/Error/Error.js';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import "./App.css";
-import NavBar from "./Components/Navbar/NavBar";
-import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <p>Hello</p>
-      <p>WORLD</p>
-      <Footer />
-    </div>
+    <main className="App">
+      <Switch>
+        <Route exact path='/' render={() => <Home/>} />
+        <Route exact path='/Booking/:id' render={({match}) => <Booking id={match.params.id}/>} />
+        <Route exact path='/Dashboard' render={() => <Dashboard/>} />
+        <Route exact path='/User/:id' render={({match}) => <Profile id={match.params.id}/>} />
+        <Route exact path='/error' component={Error} />
+        <Redirect to='/error'/>
+      </Switch>
+    </main>
   );
 }
 
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from "../../Components/Navbar/NavBar";
+import Footer from "../../Components/Footer/Footer";
+
+
+const Home = () => {
+  return (
+    <div>
+    <NavBar />
+    <p>Hello</p>
+    <p>WORLD</p>
+    <Footer />
+    </div>
+  )
+}
+
+export default Home
