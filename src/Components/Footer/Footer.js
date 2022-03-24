ab5cb4565cdbdeddfb92cb5e3115aba83cd20c34
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-03-24 13:24:44 -0400

    Create nav and footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import "../Footer/Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer">
+        <p>Copyright 2022 RÜM</p>
+        <p>Contact Us | FAQ</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
