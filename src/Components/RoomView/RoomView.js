28fd12db6053f6a793eb6dd26289452fe2a60768
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-03-29 14:40:00 -0700

    Cleaned up logic in Room View to account for loading

--- src/Components/RoomView/RoomView.js
+++ src/Components/RoomView/RoomView.js
@@ -5,62 +5,63 @@ import {getIndividualRoom} from "../../queries";
 import {useQuery} from '@apollo/client';
 
 const RoomView = (props) => {
-  const {loading, data} = useQuery(getIndividualRoom(props.id))
+  const {loading, data, error} = useQuery(getIndividualRoom(props.id))
   console.log(props.id)
-  console.log(data)
+  console.log(loading, data, error)
 
   return (
-    <div className="detailed-view-container">
-      <div className="detailed-view-card">
-        <img
-          src={auditorium}
-          className="detailed-view-photo"
-          alt="detailed-room-view"
-        />
-        <div className="below-image">
-          <div className="detailed-view-info">
-            <div className="detailed-view-top-info">
-              <div className="detailed-view-room-name-info">
-                <p className="detailed-view-info-title room-title">{data.getRoom.name}</p>
+    <>
+    {loading ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="detailed-view-container">
+        <div className="detailed-view-card">
+          <img
+            src={auditorium}
+            className="detailed-view-photo"
+            alt="detailed-room-view"
+          />
+          <div className="below-image">
+            <div className="detailed-view-info">
+              <div className="detailed-view-top-info">
+                <div className="detailed-view-room-name-info">
+                  <p className="detailed-view-info-title room-title">{data.getRoom.name}</p>
+                </div>
+                <div className="detailed-view-price-info">
+                  <div className="detailed-view-info-title price-title">Price:</div>
+                  <div className="detailed-view-rental-price price-text">${data.getRoom.price}</div>
+                </div>
+                <div className="detailed-view-available-instruments-info">
+                  <div className="detailed-view-info-title instrument-title">
+                    Available Instruments:
+                  </div>
+                  <div className="detailed-view-instruments-list instrument-text">
+                    {data.getRoom.instruments}
+                  </div>
+                </div>
+                <div className="detailed-view-amenity-info">
+                  <div className="detailed-view-info-title amenities-title">Amenities:</div>
+                  <div className="detailed-view-amenity-list amenities-text">
+                    {data.getRoom.amenities}
+                  </div>
+                </div>
               </div>
-              {/* <div className="detailed-view-ratings-info">
-                <div className="detailed-view-info-title ratings-title">Ratings:</div>
-                <div className="detailed-view-ratings-value ratings-text">4.2/5</div>
-              </div> */}
-              <div className="detailed-view-price-info">
-                <div className="detailed-view-info-title price-title">Price:</div>
-                <div className="detailed-view-rental-price price-text">${data.getRoom.price}</div>
+              <div className="detailed-view-bottom-info">
+                <div className="detailed-view-info-title description-title">Full Description:</div>
+                <div className="detailed-view-amenity-list description-text">
+                  {data.getRoom.details}
+                </div>
               </div>
+            </div>
 
-              <div className="detailed-view-available-instruments-info">
-                <div className="detailed-view-info-title instrument-title">
-                  Available Instruments:
-                </div>
-                <div className="detailed-view-instruments-list instrument-text">
-                  {data.getRoom.instruments}
-                </div>
-              </div>
-              <div className="detailed-view-amenity-info">
-                <div className="detailed-view-info-title amenities-title">Amenities:</div>
-                <div className="detailed-view-amenity-list amenities-text">
-                  {data.getRoom.amenities}
-                </div>
-              </div>
-            </div>
-            <div className="detailed-view-bottom-info">
-              <div className="detailed-view-info-title description-title">Full Description:</div>
-              <div className="detailed-view-amenity-list description-text">
-                {data.getRoom.details}
-              </div>
-            </div>
+            <Link to="/dashboard">
+              <button className="book-now-button">BOOK NOW</button>
+            </Link>
           </div>
-
-          <Link to="/dashboard">
-            <button className="book-now-button">BOOK NOW</button>
-          </Link>
         </div>
       </div>
-    </div>
+    )}
+    </>
   );
 };
 
