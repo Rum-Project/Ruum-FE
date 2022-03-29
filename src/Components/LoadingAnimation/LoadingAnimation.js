d7d7e82078d6248af22d336305d71adb095195cd
unknown <tonycarpenter@gmail.com>
2022-03-29 07:17:53 -0400

    Create loading animation

---
+++ src/Components/LoadingAnimation/LoadingAnimation.js
@@ -0,0 +1,26 @@
+import "./LoadingAnimation.css"
+
+const LoadingAnimation = () => {
+  return(
+  <div class="animation">
+    <div class="bar bar1"></div>
+    <div class="bar bar2"></div>
+    <div class="bar bar3"></div>
+    <div class="bar bar4"></div>
+    <div class="bar bar5"></div>
+    <div class="bar bar6"></div>
+    <div class="bar bar7"></div>
+    <div class="bar bar8"></div>
+    <div class="bar bar9"></div>
+    <div class="bar bar10"></div>
+    <div class="bar bar11"></div>
+    <div class="bar bar12"></div>
+    <div class="bar bar13"></div>
+    <div class="bar bar14"></div>
+    <div class="bar bar15"></div>
+    <div class="bar bar16"></div>
+  </div>
+  )
+}
+
+export default LoadingAnimation;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import RenterResultsContainer from "../../Components/RenterResultsContainer/RenterResultsContainer";
+import Login from "../../Components/Login/Login";
+import NewUserCreation from "../../Components/NewUserCreation/NewUserCreation";
+import LoadingAnimation from "../../Components/LoadingAnimation/LoadingAnimation";
 
 const Home = () => {
   return (
     <div>
-      <RenterResultsContainer />
+      <LoadingAnimation />
     </div>
   );
 };
