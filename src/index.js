3ea38c529893b5d00e100470f0e3459dba0b1f7e
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-03-28 21:45:03 -0400

    Fix linter variable

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import reportWebVitals from './reportWebVitals';
-import {ApolloProvider, ApolloClient, InMemoryCache, gql} from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://powerful-lake-27669.herokuapp.com/graphql',
-  cache: new InMemoryCache()
+  uri: "https://powerful-lake-27669.herokuapp.com/graphql",
+  cache: new InMemoryCache(),
 });
-
 
 ReactDOM.render(
   <ApolloProvider client={client}>
@@ -20,7 +19,7 @@ ReactDOM.render(
       </React.StrictMode>
     </BrowserRouter>
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
