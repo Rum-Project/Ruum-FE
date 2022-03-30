f88b6b0df053071feb53dfc321e3d9a53aaf3b29
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-03-30 15:29:50 -0400

    Add missing checkboxes in amenities

--- src/Utils/filterOptions.js
+++ src/Utils/filterOptions.js
@@ -1,18 +1,20 @@
 export const instrumentAvailableOptions = [
   { value: "drums", label: "Drums" },
   { value: "piano", label: "Piano" },
-  { value: "trumpet",label:"Trumpet"},
-  { value: "guitar",label:"Guitar"},
-  { value: "keys", label:"Keys"},
-  { value: "gangsa",label:"Gangsa"},
-  { value: "bagpipes",label:"Bagpipes"}
+  { value: "trumpet", label: "Trumpet" },
+  { value: "guitar", label: "Guitar" },
+  { value: "keys", label: "Keys" },
+  { value: "gangsa", label: "Gangsa" },
+  { value: "bagpipes", label: "Bagpipes" },
 ];
 
 export const amenitiesAvailableOptions = [
   { value: "bathrooms", label: "Restrooms" },
   { value: "drinking water", label: "Drinking Water" },
   { value: "wifi", label: "Wifi" },
-  { value: "coffee maker", label: "Coffee Maker"}
+  { value: "coffee maker", label: "Coffee Maker" },
+  { value: "bar", label: "Bar" },
+  { value: "pressure cooker", label: "Pressure Cooker" },
 ];
 
 export const sortOptions = [
