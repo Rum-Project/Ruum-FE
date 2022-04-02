eb7d0f75daae8e6a4f00a17f1db73549f7f22566
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-04-02 18:31:21 -0400

    Add past bookings logic

--- src/Components/RenterBookingsContainer/RenterBookingsContainer.js
+++ src/Components/RenterBookingsContainer/RenterBookingsContainer.js
@@ -2,7 +2,6 @@ import "./RenterBookingsContainer.css";
 import RenterBookingCard from "../RenterBookingCard/RenterBookingCard";
 
 const RenterBookingsContainer = (props) => {
-
   const getFutureBookings = () => {
     const today = new Date(new Date().toDateString());
     const futureBookings = props.bookings.filter((booking) => {
@@ -19,9 +18,35 @@ const RenterBookingsContainer = (props) => {
         return 0;
       }
     });
-    return futureBookings.map((booking) => {return <RenterBookingCard key={booking.id} id={booking.id} booking={booking}/>})
+    return futureBookings.map((booking) => {
+      return (
+        <RenterBookingCard key={booking.id} id={booking.id} booking={booking} />
+      );
+    });
+  };
 
-  }
+  const getPastBookings = () => {
+    const today = new Date(new Date().toDateString());
+    const pastBookings = props.bookings.filter((booking) => {
+      return new Date(booking.date) < today;
+    });
+    pastBookings.sort((a, b) => {
+      a = new Date(a.date);
+      b = new Date(b.date);
+      if (a < b) {
+        return 1;
+      } else if (b < a) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    return pastBookings.map((booking) => {
+      return (
+        <RenterBookingCard key={booking.id} id={booking.id} booking={booking} />
+      );
+    });
+  };
 
   const getPastBookings = () => {
     const today = new Date(new Date().toDateString());
