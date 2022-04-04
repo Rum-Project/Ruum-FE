95dbf78567c2b0ad06d434b1893874ebb4c5d31b
Daniel O'Connell <danieloconnell.maker@gmail.com>
2022-04-04 13:18:17 -0700

    Handle errors on individual room view

--- src/Pages/Booking/Booking.js
+++ src/Pages/Booking/Booking.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import RoomView from '../../Components/RoomView/RoomView'
+import Error from '../Error/Error';
 import LoadingAnimation from '../../Components/LoadingAnimation/LoadingAnimation'
 import {getIndividualRoom} from "../../queries";
 import {useQuery} from '@apollo/client';
 
 const Booking = (props) => {
-  const {loading, data} = useQuery(getIndividualRoom(props.id))
+  const {loading, data, error} = useQuery(getIndividualRoom(props.id))
 
+  const handleLoadingAndError = () => {
+    if (loading) {
+      return <LoadingAnimation />
+    } else if (error) {
+      return <Error />
+    } else {
+      return <RoomView room={data.getRoom} date={props.date}/>
+    }
+  }
 
   return (
     <>
-      {loading ? (
-        <LoadingAnimation />
-      ) :
-      (<RoomView room={data.getRoom} date={props.date}/>
-    )}
+      {handleLoadingAndError()}
     </>
   )
 }
